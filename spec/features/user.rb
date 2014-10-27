0dc476475403c4c95d6e8b3ff9f5629b685d79fa
Dax <d.dax@email.com>
2014-10-27 23:53:27 +0100

    Refactor user test

--- spec/features/user.rb
+++ spec/features/user.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 class FeaturesUser < FeatureTest
 
   it "registers new users" do
-    user = authenticate_test_user
+    user = create_test_user
 
     assert_equal '1',             user.uid
     assert_equal 'test_user',     user.nickname
@@ -15,18 +15,10 @@ class FeaturesUser < FeatureTest
     create_more_users
     assert_equal 5,               get_users.users.count
 
-    update_terms_accepted(user.uid)
     user = get_user(user.uid)
 
-    assert_equal '1',             user.uid
-    assert_equal 'test_user',     user.nickname
-    assert_nil                    user.email
-    assert_empty                  user.favorites
-    assert_empty                  user.added
-    assert_equal true,            user.terms_accepted
-
-    assert_denied                 update_user(:uid      => user.uid,
-                                              :nickname => 'user_two')
+    assert_denied update_user(:uid      => user.uid,
+                              :nickname => 'user_two')
 
     update_user(:uid      => user.uid,
                 :nickname => 'new_user_one',
@@ -41,24 +33,34 @@ class FeaturesUser < FeatureTest
     assert_empty                  user.added
     assert_equal true,            user.terms_accepted
 
+    assert_equal user.uid, authenticate_test_user
+  end
 
+  def create_test_user(options = {})
+    call_use_case(Users, :CreateUser,
+      :user => {
+        :nickname   => options[:nickname]   ||'nickname',
+        :email      => options[:email]      ||'email',
+        :auth_key   => options[:auth_key]   ||'auth_key'
+      }
+    )
   end
 
   def authenticate_test_user(options = {})
-    call_use_case(:Users, :AuthenticateUser,
+    call_use_case(Users, :AuthenticateUser,
       :nickname => options[:nickname] || 'test_user',
       :auth_key => options[:auth_key] || 'auth_key'
     )
   end
 
   def get_user(uid)
-    call_use_case(:Users, :GetUser,
+    call_use_case(Users, :GetUser,
       :uid => uid
     )
   end
 
   def create_quote_for_user(user_uid)
-    call_use_case(:Quotes, :CreateQuote,
+    call_use_case(Quotes, :CreateQuote,
       :added_by   => user_uid,
       :quote      => {
         :author   => "Author for Quote ##{i+1}",
@@ -69,10 +71,10 @@ class FeaturesUser < FeatureTest
   end
 
   def create_more_users
-    authenticate_test_user(:nickname => 'user_two',   :password => 'weak_sauce')
-    authenticate_test_user(:nickname => 'user_three', :password => 'jesus_bros')
-    authenticate_test_user(:nickname => 'user_four',  :password => '420_4_ever')
-    authenticate_test_user(:nickname => 'user_five',  :password => 'lewinsky_m')
+    create_test_user(:nickname => 'user_two',   :password => 'weak_sauce')
+    create_test_user(:nickname => 'user_three', :password => 'jesus_bros')
+    create_test_user(:nickname => 'user_four',  :password => '420_4_ever')
+    create_test_user(:nickname => 'user_five',  :password => 'lewinsky_m')
   end
 
 end
